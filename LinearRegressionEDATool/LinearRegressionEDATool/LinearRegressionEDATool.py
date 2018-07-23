2c03775243cf3983806e6817b72ded7b784f3ed2
Patrick Ignoto <patrick.ignoto@gmail.com>
2018-07-23 14:26:35 -0400

    Added many linear regression models that can be passed as part of the tool

--- LinearRegressionEDATool/LinearRegressionEDATool/LinearRegressionEDATool.py
+++ LinearRegressionEDATool/LinearRegressionEDATool/LinearRegressionEDATool.py
@@ -12,11 +12,50 @@ def linear_regression_eda_tool(features, target, requested_regressions=['Linear'
         if r == 'Linear':
             from sklearn.linear_model import LinearRegression
             regression_models[r]=LinearRegression()
-        elif r == 'LASSO':
+        elif r == 'Ridge':
+            from sklearn.linear_model import Ridge
+            regression_models[r]=Ridge()
+        elif r == 'Lasso':
             from sklearn.linear_model import Lasso
             regression_models[r]=Lasso()
+        elif r == 'MultiTaskLasso':
+            from sklearn.linear_model import MultiTaskLasso
+            regression_models[r]=MultiTaskLasso()
+        elif r == 'ElasticNet':
+            from sklearn.linear_model import ElasticNet
+            regression_models[r]=ElasticNet()
+        elif r == 'MultiTaskElasticNet':
+            from sklearn.linear_model import MultiTaskElasticNet
+            regression_models[r]=MultiTaskElasticNet()
+        elif r == 'Lars':
+            from sklearn.linear_model import Lars
+            regression_models[r]=Lars()
+        elif r == 'LassoLars':
+            from sklearn.linear_model import LassoLars
+            regression_models[r]=LassoLars()
+        elif r == 'OrthogonalMatchingPursuit':
+            from sklearn.linear_model import OrthogonalMatchingPursuit
+            regression_models[r]=OrthogonalMatchingPursuit()
+        elif r == 'BayesianRidge':
+            from sklearn.linear_model import BayesianRidge
+            regression_models[r]=BayesianRidge()
+        elif r == 'ARDRegression':
+            from sklearn.linear_model import ARDRegression
+            regression_models[r]=ARDRegression()
+        elif r == 'SGDRegressor':
+            from sklearn.linear_model import SGDRegressor
+            regression_models[r]=SGDRegressor()
+        elif r == 'RANSACRegressor':
+            from sklearn.linear_model import RANSACRegressor
+            regression_models[r]=RANSACRegressor()
+        elif r == 'TheilSenRegressor':
+            from sklearn.linear_model import TheilSenRegressor
+            regression_models[r]=TheilSenRegressor()
+        elif r == 'HuberRegressor':
+            from sklearn.linear_model import HuberRegressor
+            regression_models[r]=HuberRegressor()
         else:
-            print(r + " is an unsupported regression type")
+            print(r + " is an unsupported regression type. Check if you have misspelled the name.")
 
     i = 1
     for m in regression_models:
