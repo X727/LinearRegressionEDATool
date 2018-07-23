d9a235506105c1b544c91f30e727afcf55abfeee
Patrick Ignoto <patrick.ignoto@gmail.com>
2018-07-23 11:26:09 -0400

    Added plot_learning_curve method and set structure to plot learning curve for multiple regression types
    Added the Plot_learning_curve method from the scikit-learn documentation to easily plot learning curves.  Added structure to allow for iterating over multiple regression types and ploted learning curve for linear regression.

--- LinearRegressionEDATool/LinearRegressionEDATool/LinearRegressionEDATool.py
+++ LinearRegressionEDATool/LinearRegressionEDATool/LinearRegressionEDATool.py
@@ -2,7 +2,23 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+from sklearn.model_selection import ShuffleSplit
+import plot_learning_curve
 
-def LinearRegressionEDATool(features, target):
-    print(features.head())
-    print(target)
+def LinearRegressionEDATool(features, target, requested_regressions=['Linear']):
+    
+    regression_models = []
+    for r in requested_regressions:
+        if r == 'Linear':
+            from sklearn.linear_model import LinearRegression
+            regression_models.append(LinearRegression())
+        else:
+            print(r + " is an unsupported regression type")
+
+    for m in regression_models:
+       title = "Learning Curve"
+       cv = ShuffleSplit(n_splits=100, test_size=0.2, random_state=0)
+       lc = plot_learning_curve.plot_learning_curve(m, title, features, target, ylim=(0.1, 0.9), cv=cv, n_jobs=4)
+       lc.show()
+
+
--- LinearRegressionEDATool/LinearRegressionEDATool/main.py
+++ LinearRegressionEDATool/LinearRegressionEDATool/main.py
@@ -10,6 +10,7 @@ sns.set_style("whitegrid")
 def main():
     boston_data = load_boston()
     boston_data_features = pd.DataFrame(boston_data.data, columns=boston_data.feature_names)
+    #boston_data_features =  boston_data_features.values.reshape(-1,1)
     boston_data_target = boston_data.target
     LinearRegressionEDATool.LinearRegressionEDATool(boston_data_features,boston_data_target)
 
